76eb723a6b736def5fa8ccd6c6d4c954dac6fa91
Admin <ronitsanghvi2000@gmail.com>
2024-06-07 20:32:20 -0400

    remove '

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         </div>
         <div className="flex flex-1 flex-col text-left px-28 justify-center whitespace-nowrap" id="logo">
           <span className="text-2xl text-black tracking-widest font-bold">AJOE.ORG</span>
-          <text className="text-black tracking-widest"><text className="text-red-600 font-bold">A</text>ssociation des <text className="text-blue-600 font-bold">J</text>uifs <text className="text-blue-600 font-bold">O</text>riginaires d'<text className="text-red-600 font-bold">E</text>gypte	</text>
+          <text className="text-black tracking-widest"><text className="text-red-600 font-bold">A</text>ssociation des <text className="text-blue-600 font-bold">J</text>uifs <text className="text-blue-600 font-bold">O</text>riginaires d&apos;<text className="text-red-600 font-bold">E</text>gypte	</text>
         </div>
         <div className="p-0 pr-2 h-104 flex">
           <Image alt="carte_web" src="/carte_web.jpeg" width="122" height="104" className="object-cover" />
